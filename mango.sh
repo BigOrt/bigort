c0b95a011252a7f92a3cf7e2da744810781482db
Big_ort <adytamasurya@gmail.com>
2019-02-23 01:45:52 +0700

    Update mango.sh

--- mango.sh
+++ mango.sh
@@ -3,11 +3,11 @@ git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
-sudo add-apt-repository ppa:jonathonf/gcc-7.1
-sudo apt-get update
-sudo apt-get install gcc-7 g++-7
-cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
-#cmake ..
+#sudo add-apt-repository ppa:jonathonf/gcc-7.1
+#sudo apt-get update
+#sudo apt-get install gcc-7 g++-7
+#cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
+cmake ..
 make
 
 sudo cp xmrig /usr/local/bin
