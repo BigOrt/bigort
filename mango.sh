0cb759a5cd2b47d4e668a808da2354a4c35154e4
Big_ort <adytamasurya@gmail.com>
2019-02-23 00:44:20 +0700

    Update mango.sh

--- mango.sh
+++ mango.sh
@@ -4,6 +4,7 @@ cd xmrig
 mkdir build
 cd build
 sudo add-apt-repository ppa:jonathonf/gcc-7.1
+sudo apt-get update
 sudo apt-get install gcc-7 g++-7
 cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
 make
