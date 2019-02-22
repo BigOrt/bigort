58832d0db3efe43eb162660ef925ca2d1cf6b0f7
Big_ort <adytamasurya@gmail.com>
2019-02-22 23:33:42 +0700

    Update mango.sh

--- mango.sh
+++ mango.sh
@@ -1,9 +1,11 @@
-sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
+sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev 
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
-cmake ..
+sudo add-apt-repository ppa:jonathonf/gcc-7.1
+sudo apt-get install gcc-7 g++-7
+cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
 make
 
 cp xmrig /usr/local/bin
