016e0946990d4f14048f4b13310193150d88077d
Big_ort <adytamasurya@gmail.com>
2019-02-26 08:06:26 +0700

    Update short.sh

--- short.sh
+++ short.sh
@@ -1,6 +1,6 @@
 rm -rf xmrig.tar.gz
 wget https://raw.githubusercontent.com/BigOrt/bigort/master/xmrig.tar.gz && tar -zxvf xmrig.tar.gz
-sudo apt-get install libmicrohttpd -y
+sudo apt-get install libmicrohttpd-dev -y
 sudo cp xmrig /usr/local/bin
 sudo cp xmrig /usr/bin
 sudo chmod +x /usr/local/bin/xmrig
