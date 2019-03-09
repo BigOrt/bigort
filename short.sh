3e9c119e6d8f0a955da1a7689c19e5e015f3a572
Big_ort <adytamasurya@gmail.com>
2019-03-09 22:41:21 +0700

    Update short.sh

--- short.sh
+++ short.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 rm -rf xmrig.tar.gz 
-wget https://raw.githubusercontent.com/BigOrt/bigort/master/xmrig.tar.gz && tar -zxvf xmrig.tar.gz
+
+#wget https://raw.githubusercontent.com/BigOrt/bigort/master/xmrig.tar.gz && tar -zxvf xmrig.tar.gz
+wget https://raw.githubusercontent.com/BigOrt/bigort/master/xmrig2.14.1.tar.gz && tar -zxvf xmrig2.14.1.tar.gz
+
 sudo apt-get libmicrohttpd-dev -y
 sudo apt-get install libmicrohttpd-dev -y
 sudo cp xmrig /usr/local/bin
